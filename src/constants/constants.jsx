a9595225290b90a786ee6f31583ca664856ef00a
CokdePutra <cokgdeputrawidnyana25@gmail.com>
2024-12-07 12:18:53 +0800

    adding review page

--- src/constants/constants.jsx
+++ src/constants/constants.jsx
@@ -216,14 +216,3 @@ export const ServiceDetail = [
     id: "16",
   },
 ];
-
-export const Review = [
-  {
-    text: "Awesome Driver",
-    name: "Aron Smith",
-    desc: "Lorem ipsum is siply dummy text",
-    image: "./images/hero_image1.png",
-    profilePicture: "./images/hero_image1.png",
-    id: "1",
-  },
-];
